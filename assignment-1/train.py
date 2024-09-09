4a200642c12eec818e05110f5ef8229d915ab162
SRIVATSA SINHA <srivatsasinha@Srivatsas-MacBook-Pro.local>
2024-09-10 01:06:04 +0530

    refactored training code

--- assignment-1/train.py
+++ assignment-1/train.py
@@ -17,9 +17,13 @@ if parent_dir not in sys.path:
     sys.path.append(parent_dir)
 
 from config import TrainConfig, Config
+from models.lstm import LSTMLanguageModel
+from models.n_gram import NGramLanguageModel
+from models.transformer import TSGLangauageModel
 
 ## Constants
 DATASET_PATH = 'dataset/processed/Auguste_Maquet.txt'
+MODEL_CKPT_DIRECTORY = 'trained_model'
 BLOCK_SIZE = 50
 NUM_GPU = 1
 READ_ORIGINAL = True
@@ -69,6 +73,7 @@ def generate_labeled_data(dataset_cleaned,train_size,test_size,context_size,left
 
 ## Section 2: Read the Training Data ##
 
+# Sliding Window Implementation
 # Data Generator: Creates X,Y pairs for training where X is the  input token idices for a given context window and Y is the target indices
 # Y is shifted version of X by one token when ran over entire corpus.
 def get_set(data,block_size):
@@ -76,60 +81,59 @@ def get_set(data,block_size):
     y = torch.stack([data[i+1:i+block_size+1] for i in range(0,len(data)-block_size-2)])
     return x,y
 
-with open(DATASET_PATH, 'r') as f:
-    dataset = f.readlines()
 
-dataset_cl = None
+def load_data_and_get_pytorch_dataloaders(dataset_path, batch_size, block_size, num_gpu, encoder, clean_data = False):
 
-if not READ_ORIGINAL:
-    dataset_cl = clean_data(dataset)
-else:
-    dataset_cl = ' '.join(dataset)
+    with open(dataset_path, 'r') as f:
+        dataset = f.readlines()
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-encode = lambda s: tokenizer.encode(s)
-decode = lambda l: tokenizer.decode(l)
+    dataset_cl = None
 
-# Train Test Split 
-data = torch.tensor(encode(dataset_cl), dtype=torch.long)
-n = int(0.7*len(data)) 
-e = int(0.9*len(data))
-train_data = data[:n]  # 70% Data for Training
-eval_data = data[n:e]  # 20% Data for Evaluation
-test_data = data[e:]   # 10% Data for Testing
+    if clean_data:
+        dataset_cl = clean_data(dataset)
+    else:
+        dataset_cl = ' '.join(dataset)
+
+    # Train Test Split 
+    data = torch.tensor(encoder(dataset_cl), dtype=torch.long)
+    n = int(0.7*len(data)) 
+    e = int(0.9*len(data))
+    train_data = data[:n]  # 70% Data for Training
+    eval_data = data[n:e]  # 20% Data for Evaluation
+    test_data = data[e:]   # 10% Data for Testing
 
 
-# Create Dataloaders
-train_set = get_set(train_data,BLOCK_SIZE)
-eval_set = get_set(eval_data,BLOCK_SIZE)
-test_set = get_set(test_data,BLOCK_SIZE)
+    # Create Dataloaders
+    train_set = get_set(train_data,block_size)
+    eval_set = get_set(eval_data,block_size)
+    test_set = get_set(test_data,block_size)
 
-class TSGDatasetTrain(Dataset):
-    def __len__(self):
-        return len(train_set[0])
-    def __getitem__(self,idx):
-        return train_set[0][idx],train_set[1][idx]
+    class TSGDatasetTrain(Dataset):
+        def __len__(self):
+            return len(train_set[0])
+        def __getitem__(self,idx):
+            return train_set[0][idx],train_set[1][idx]
 
-class TSGDatasetEval(Dataset):
-    def __len__(self):
-        return len(eval_set[0])
-    def __getitem__(self,idx):
-        return eval_set[0][idx],eval_set[1][idx]
+    class TSGDatasetEval(Dataset):
+        def __len__(self):
+            return len(eval_set[0])
+        def __getitem__(self,idx):
+            return eval_set[0][idx],eval_set[1][idx]
 
-class TSGTestDataset(Dataset):
-    def __len__(self):
-        return len(test_set[0])
-    def __getitem__(self,idx):
-        return test_set[0][idx],test_set[1][idx]
-
+    class TSGTestDataset(Dataset):
+        def __len__(self):
+            return len(test_set[0])
+        def __getitem__(self,idx):
+            return test_set[0][idx],test_set[1][idx]
     
-    
-def get_dataloader(block_size, batch_size):
     train_dataset = TSGDatasetTrain()
     eval_dataset = TSGDatasetEval()
-    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers = NUM_GPU)
-    eval_dataloader = DataLoader(eval_dataset, shuffle=False, batch_size=batch_size*2, num_workers = NUM_GPU)
-    return train_dataloader, eval_dataloader
+    test_data = TSGTestDataset()
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers = num_gpu)
+    eval_dataloader = DataLoader(eval_dataset, shuffle=False, batch_size=batch_size*2, num_workers = num_gpu)
+    test_dataloader = DataLoader(test_dataloader,shuffle=False,batch_size=batch_size*2, num_workers = num_gpu)
+    return train_dataloader, eval_dataloader, test_dataloader
+
 
 ## Section 3: Training ##
 
@@ -139,10 +143,24 @@ def get_dataloader(block_size, batch_size):
 The following function trains a given language model with HF's Accelerate Framework using DDP.
 '''
 def train_loop(model_out):
+
+    if not os.path.exists(MODEL_CKPT_DIRECTORY):
+        os.makedirs(MODEL_CKPT_DIRECTORY)
+
     train_now = True
+    ## Intialise Accelerator with W&B Logging
     accelerator = Accelerator(log_with="wandb")
+    num_gpus = accelerator.num_processes  # Determine the number of GPUs/processes
+
     resume_from_checkpoint = False
     chk_path = 'model_0.pth'
+
+    ## Initialise Tokeniser
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    encode = lambda s: tokenizer.encode(s)
+    decode = lambda l: tokenizer.decode(l)
+
+    ## Initialise Train and Test Config
     config = Config(block_size=BLOCK_SIZE, tokenizer=tokenizer)
     train_config = TrainConfig(accelerator)
 
@@ -161,7 +179,13 @@ def train_loop(model_out):
             model.train()
             return total_loss / total_iteration
                 
-        train_dataloader, eval_dataloader = get_dataloader(config.block_size, config.batch_size)
+        train_dataloader, eval_dataloader, test_dataloader = load_data_and_get_pytorch_dataloaders(DATASET_PATH,
+                                                                                  config.batch_size,
+                                                                                  config.block_size,
+                                                                                  num_gpus,
+                                                                                  encode,
+                                                                                  not READ_ORIGINAL
+                                                                                  )
         torch.manual_seed(42)
         print(len(train_dataloader),len(eval_dataloader))
         # Initialize or load model
@@ -183,7 +207,6 @@ def train_loop(model_out):
                 project_name="anlp-assignment-1",
                 config={ **config.__dict__, **train_config.__dict__}
             )
-            num_gpus = accelerator.num_processes  # Determine the number of GPUs/processes
             for epoch in range(train_config.epochs):
                 accelerator.print(f"Epoch {epoch+1}/{train_config.epochs}")
                 
@@ -206,9 +229,15 @@ def train_loop(model_out):
                 
                 # Save model checkpoint
                 model_xp = accelerator.unwrap_model(model)
-                accelerator.save(model_xp, f"{experiment_name}_epoch_{epoch+1}.pth")
+                accelerator.save(model_xp, os.path.join(MODEL_CKPT_DIRECTORY,f"{experiment_name}_epoch_{epoch+1}.pth"))
+            test_loss = estimate_loss(model,test_dataloader,num_gpus)
+            accelerator.print(f"Final Test Set loss {test_loss:.4f}")
             accelerator.end_training()
     except:
         pass
     finally:
         accelerator.end_training()
+
+
+if __name__ == "__main__":
+    train_loop()
---
+++ chk.py
@@ -0,0 +1,3 @@
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__))) 
+print(parent_dir)
