dd9fae58c7263b8ffd88117958af51d6777b6675
SRIVATSA SINHA <srivatsasinha@Srivatsas-MacBook-Pro.local>
2024-09-28 17:01:47 +0530

    add batch dim

--- 2024701003_assignment2/generate.py
+++ 2024701003_assignment2/generate.py
@@ -10,9 +10,9 @@ tokeniser = AutoTokenizer.from_pretrained(os.path.join(MODEL_CKPT_DIRECTORY,TOKE
 trained_model = torch.load(os.path.join(MODEL_CKPT_DIRECTORY,TOKENISER_NAME)).to('cuda')
 
 EN_SENT = ''
-EN_SENT_ENC  = tokeniser(EN_SENT, return_tensors='pt', padding=True, truncation=True,max_length=50)['input_ids']
+EN_SENT_ENC  = tokeniser([EN_SENT], return_tensors='pt', padding=True, truncation=True,max_length=50)['input_ids']
 FR_SENT = start_token
-FR_SENT_ENC = tokeniser(EN_SENT, return_tensors='pt', padding=True, truncation=True,max_length=50)['input_ids']
+FR_SENT_ENC = tokeniser([FR_SENT], return_tensors='pt', padding=True, truncation=True,max_length=50)['input_ids']
 generated = tokeniser.decode(trained_model.generate(EN_SENT_ENC, FR_SENT_ENC, 50), skip_special_tokens=False)
 print(generated)
 
